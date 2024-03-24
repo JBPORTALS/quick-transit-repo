e30daa1b719e36c3eb06aa1280a241fcdfcf4ddf
Manoj <manu48617@gmail.com>
2024-03-24 14:48:24 +0530

    fix: bottom nav focus item style

--- apps/partner/src/app/(auth)/(tabs)/_layout.tsx
+++ apps/partner/src/app/(auth)/(tabs)/_layout.tsx
@@ -6,6 +6,8 @@ import { Link, Redirect, Tabs, useRouter } from "expo-router";
 import { useAuth, useUser } from "@clerk/clerk-expo";
 import { BellIcon, HomeIcon, ListIcon, TruckIcon } from "lucide-react-native";
 
+import NavItem from "~/components/nav-item";
+
 export default function TabLayout() {
   const { user } = useUser();
 
@@ -15,6 +17,7 @@ export default function TabLayout() {
         screenOptions={{
           tabBarActiveTintColor: "#A83287",
           tabBarShowLabel: false,
+          tabBarStyle: { height: 60 },
           header: (props) => {
             return (
               <View className="flex h-16 w-full flex-row  items-center justify-between bg-white px-4 shadow-md">
@@ -40,21 +43,37 @@ export default function TabLayout() {
           name="index"
           options={{
             title: "Home",
-            tabBarIcon: ({ color }) => <HomeIcon size={24} {...{ color }} />,
+            tabBarIcon: ({ color, focused }) => (
+              <NavItem {...{ focused }}>
+                <HomeIcon size={24} {...{ color }} />
+              </NavItem>
+            ),
           }}
         />
         <Tabs.Screen
           name="packages"
           options={{
             title: "Packages",
-            tabBarIcon: ({ color }) => <ListIcon size={24} {...{ color }} />,
+            tabBarIcon: ({ color, focused }) => (
+              <NavItem {...{ focused }}>
+                <ListIcon size={24} {...{ color }} />
+              </NavItem>
+            ),
           }}
         />
         <Tabs.Screen
           name="notifications"
           options={{
             title: "Notifications",
-            tabBarIcon: ({ color }) => <BellIcon size={24} {...{ color }} />,
+            tabBarIcon: ({ color, focused }) => (
+              <NavItem {...{ focused }}>
+                <BellIcon
+                  className="bg-primary/30 p-3"
+                  size={24}
+                  {...{ color }}
+                />
+              </NavItem>
+            ),
           }}
         />
       </Tabs>
