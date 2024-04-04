33b08a83b52f688ab53e0049e0a61fd0aaff59c5
JB Solutions <164311049+jb-solutions-blr@users.noreply.github.com>
2024-04-04 14:06:48 +0530

    add final block with exit code

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,16 +1,19 @@
+import { faker } from "@faker-js/faker";
 
-import { faker } from '@faker-js/faker';
-import { db } from '.';
-import { users } from './schema/users';
+import { db } from ".";
+import { users } from "./schema/users";
 
 async function main() {
-    for (let i = 0; i < 10; i++) {
-        await db.insert(users).values({
-            name: faker.person.fullName(),
-            phone_number: faker.phone.number(),
-            role: faker.helpers.arrayElement(["manager", "customer", "partner"]),
-        })
-    }
+  for (let i = 0; i < 10; i++) {
+    await db.insert(users).values({
+      name: faker.person.fullName(),
+      phone_number: faker.phone.number(),
+      role: faker.helpers.arrayElement(["manager", "customer", "partner"]),
+    });
+  }
 }
 
-main().then(() => console.log("Data has been seed ✅")).catch(() => console.log("Something went wrong ❌"))
+main()
+  .then(() => console.log("Data has been seed ✅"))
+  .catch(() => console.log("Something went wrong ❌"))
+  .finally(() => process.exit(0));
