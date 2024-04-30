2fd8e4cdcaabee29d5392cb8ec191f6f80d355c6
x-sss-x <manu48167@gmail.com>
2024-04-30 22:30:38 +0530

    "Update pickUp-partner page: replace Payment type with Partner, update columns and cell rendering"
    This commit updates the `pickUp-partner` page by replacing the `Payment` type with a new `Partner` type

--- apps/manager/src/app/(auth)/(with-sidebar)/pickUp-partner/columns.tsx
+++ apps/manager/src/app/(auth)/(with-sidebar)/pickUp-partner/columns.tsx
@@ -4,101 +4,95 @@ import Image from "next/image";
 import Link from "next/link";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, Star, StarHalf } from "lucide-react";
+import { cn } from "node_modules/@qt/ui/lib/utils";
 
-import { Button } from "@qt/ui/button";
+import { Avatar, AvatarImage } from "@qt/ui/avatar";
+import { Button, buttonVariants } from "@qt/ui/button";
 import { Package, PackageBody, PackageThumbneil } from "@qt/ui/package";
+import { HStack, VStack } from "@qt/ui/stack";
 import { Text } from "@qt/ui/text";
 
-export type Payment = {
-    id: string;
-    email: string;
-    fullname: string;
-    phoneNo:number;
-    packagesDelivered:number;
-    rating:number
+export type Partner = {
+  id: string;
+  email: string;
+  fullname: string;
+  phoneNo: number;
+  packagesDelivered: number;
+  rating: number;
 };
 
 // Function to return JSX tag based on status
 
+export const columns: ColumnDef<Partner>[] = [
+  {
+    accessorKey: "partnerID",
+    header: "Partner",
+    cell({ row }) {
+      const { id, fullname, email } = row.original as Partner;
+      return (
+        <Link href={`/partner-details/${id}`}>
+          <HStack
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              "h-fit w-full max-w-full justify-start",
+            )}
+          >
+            <Avatar>
+              <AvatarImage src="https://github.com//shadcn.png" />
+            </Avatar>
+            <VStack className="gap-1">
+              <Text styles={"small"}>{fullname}</Text>
+              <Text styles={"details"} className="text-muted-foreground">
+                {email}
+              </Text>
+            </VStack>
+          </HStack>
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "phoneNo",
+    header: "Phone no",
+  },
+  {
+    accessorKey: "packagesDelivered",
+    header: "Packages Delivered",
+    cell({ row }) {
+      const { packagesDelivered } = row.original as Partner;
+      return <div className="flex justify-center">{packagesDelivered}</div>;
+    },
+  },
+  {
+    accessorKey: "averageRating",
+    header: "Average Rating",
+    cell({ row }) {
+      const { rating } = row.original as Partner;
 
-export const columns: ColumnDef<Payment>[] = [
-    {
-        accessorKey: "slno",
-        header: "SL NO",
-        cell: ({ row }) => row.index + 1,
-    },
-    {
-        accessorKey: "partnerID",
-        header: "Partner ID",
-        cell({ row }) {
-            const { id } = row.original as Payment;
-            return (
-                <Link href={`/partner-details/${id}`}>
-                    <Package>
-                        <PackageBody className="flex flex-col gap-1">
-                            <Text styles={"table_item"}>{id}</Text>
-                        </PackageBody>
-                    </Package>
-                </Link>
-            );
-        },
-    },
-    {
-        accessorKey: "fullname",
-        header: "Full Name",
-      },
-    {
-        accessorKey: "email",
-        header: "Email",
-      },
-      {
-        accessorKey: "phoneNo",
-        header: "Phone no",
-      },
-      {
-        accessorKey: "packagesDelivered",
-        header: "Packages Delivered",
-        cell({row}){
-            const { packagesDelivered } = row.original as Payment;
-            return(
-                <div className="flex justify-center">
-                    {packagesDelivered}
-                </div>
-            )
+      // Function to generate stars based on rating
+      const renderStars = (rating: number) => {
+        const fullStars = Math.floor(rating);
+        const halfStar = rating - fullStars >= 0.5;
+        const stars = [];
+        for (let i = 0; i < fullStars; i++) {
+          stars.push(<Star key={i} size={15} className="text-yellow-600" />);
         }
-      },
-      {
-        accessorKey: "averageRating",
-        header: "Average Rating",
-        cell({ row }) {
-            const { rating, id } = row.original as Payment;
-    
-            // Function to generate stars based on rating
-            const renderStars = (rating: number) => {
-                const fullStars = Math.floor(rating);
-                const halfStar = rating - fullStars >= 0.5;
-                const stars = [];
-                for (let i = 0; i < fullStars; i++) {
-                    stars.push(<Star key={i} size={15}/>);
-                }
-                if (halfStar) {
-                    stars.push(<StarHalf key={fullStars} size={15}  />);
-                }
-                return stars;
-            };
-    
-            return (
-                <Link href={`/request-details/${id}`}>
-                    <Package>
-                        <PackageBody className="flex flex-col gap-1">
-                            {/* Render stars based on rating */}
-                            <Text styles={"table_item"} className='flex'>
-                                {renderStars(rating)}
-                            </Text>
-                        </PackageBody>
-                    </Package>
-                </Link>
-            );
-        },
-    } 
+        if (halfStar) {
+          stars.push(
+            <StarHalf key={fullStars} size={15} className="text-yellow-600" />,
+          );
+        }
+        return stars;
+      };
+
+      return (
+        <PackageBody className="flex flex-col gap-1">
+          {/* Render stars based on rating */}
+          <Text styles={"table_item"} className="flex">
+            {renderStars(rating)}
+          </Text>
+        </PackageBody>
+      );
+    },
+  },
 ];
