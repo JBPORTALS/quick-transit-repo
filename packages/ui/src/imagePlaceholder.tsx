6fab7b07fe4a3445a1ff2f7fbc087313d72c2c2d
x-sss-x <manu48167@gmail.com>
2024-04-30 15:30:26 +0530

    improve placeholder

--- packages/ui/src/imagePlaceholder.tsx
+++ packages/ui/src/imagePlaceholder.tsx
@@ -1,9 +1,27 @@
-"use client"
-import { Image } from 'lucide-react';
-import React, { useState, useRef } from 'react';
+"use client";
 
-const ImageUploader = () => {
-  const [image, setImage] = useState<string | null>(null);
+import React, { useRef, useState } from "react";
+import { Image } from "lucide-react";
+import {
+  ControllerProps,
+  ControllerRenderProps,
+  FieldPath,
+  FieldValues,
+} from "react-hook-form";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import { Input } from "./input";
+
+const ImageUploader = ({
+  ...props
+}: Omit<ControllerRenderProps, "ref" | "onChange">) => {
+  const [image, setImage] = useState<string | null>(props.value);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,11 +29,12 @@ const ImageUploader = () => {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        if (typeof reader.result === 'string') {
+        if (typeof reader.result === "string") {
           setImage(reader.result);
         }
       };
       reader.readAsDataURL(file);
+      console.log(file);
     }
   };
 
@@ -25,7 +44,7 @@ const ImageUploader = () => {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        if (typeof reader.result === 'string') {
+        if (typeof reader.result === "string") {
           setImage(reader.result);
         }
       };
@@ -44,25 +63,45 @@ const ImageUploader = () => {
   };
 
   return (
-      <div className="w-64 h-52 border-2 border-gray-300 border-dashed rounded-lg flex items-center justify-center" onClick={handleDivClick} onDrop={handleDrop} onDragOver={handleDragOver}>
-        {image ? (
-          <img src={image} alt="Uploaded" className="max-h-full max-w-full" />
-        ) : (
-          <div className='flex flex-col gap-7 items-center'>
-            <Image />
-            <div>
-              <h1 className="text-sm font-medium">Drop your image here or select </h1>
-              <span className="text-muted-foreground text-sm flex justify-center font-medium">click to browse</span>
+    <FormItem className="w-full">
+      <FormLabel>{"Picture 3 (optional)"}</FormLabel>
+      <FormControl>
+        <div
+          className="flex h-52 w-full items-center justify-center rounded-lg border-2 border-dashed border-gray-300"
+          onClick={handleDivClick}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          {props.value ? (
+            <img
+              src={props.value}
+              alt="Uploaded"
+              className="max-h-full max-w-full"
+            />
+          ) : (
+            <div className="flex flex-col items-center gap-7">
+              <Image />
+              <div>
+                <h1 className="text-sm font-medium">
+                  Drop your image here or select
+                </h1>
+                <span className="flex justify-center text-sm font-medium text-muted-foreground">
+                  click to browse
+                </span>
+              </div>
             </div>
-          </div>
-        )}
-        <input
-          ref={fileInputRef}
-          type="file"
-          className="hidden"
-          onChange={handleFileInputChange}
-        />
-      </div>
+          )}
+          <Input
+            type="file"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleFileInputChange}
+            {...props}
+          />
+        </div>
+      </FormControl>
+      <FormMessage />
+    </FormItem>
   );
 };
 
