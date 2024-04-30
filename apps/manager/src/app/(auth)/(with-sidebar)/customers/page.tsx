367564ea8a88a9767a8a2f3ef3570de3e57769c0
x-sss-x <manu48167@gmail.com>
2024-04-30 22:47:14 +0530

    few changes

--- apps/manager/src/app/(auth)/(with-sidebar)/customers/page.tsx
+++ apps/manager/src/app/(auth)/(with-sidebar)/customers/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function page() {
-  return <div>Customer Table goes here ...</div>;
+  return (
+    <div>Customer Table goes here (During Backend Integration Time) ...</div>
+  );
 }
