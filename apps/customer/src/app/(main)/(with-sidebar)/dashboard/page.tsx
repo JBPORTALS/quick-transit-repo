3ca15f1ffd9d768fc4afdb1a4ca498b1d59afb2a
x-sss-x <manu48167@gmail.com>
2024-05-11 12:21:37 +0530

    "Remove border and hover effect from TableRow; update global CSS variables"
    This commit removes the border and hover effect from the TableRow component and updates the global CSS variables for card, popover, and input to have a lighter background

--- apps/customer/src/app/(main)/(with-sidebar)/dashboard/page.tsx
+++ apps/customer/src/app/(main)/(with-sidebar)/dashboard/page.tsx
@@ -15,77 +15,13 @@ import { Text } from "@qt/ui/text";
 export default function page() {
   return (
     <div className="grid grid-cols-3 gap-5">
-      <div className="col-span-2 flex-1  rounded-radius border bg-card p-6 py-6 shadow-sm">
-        <Text styles={"large"} className=" text-secondary-foreground ">
+      <div className="col-span-2">
+        <Text
+          styles={"p_ui_medium"}
+          className="font-medium text-muted-foreground "
+        >
           Your Recent Requests
         </Text>
-        <div className="py-6">
-          <div className="py-3.5">
-            <Requests variant={"default"} className=" border">
-              <RequestBody>
-                <HStack className="py-[7px]">
-                  <RequestImage />
-                </HStack>
-                <VStack className="gap-1">
-                  <RequestLabel>Question Papers</RequestLabel>
-                  <HStack>
-                    <RequestWeightLabel>25x20x25</RequestWeightLabel>
-                    <RequestWeightLabel>2Kg</RequestWeightLabel>
-                  </HStack>
-                  <RequestTimeLabel>
-                    <CalendarDays height={16} width={16} className="mr-1" />{" "}
-                    Requested 5 hours ago...
-                  </RequestTimeLabel>
-                </VStack>
-              </RequestBody>
-              <RequestButton />
-            </Requests>
-          </div>
-
-          <div className="py-3.5">
-            <Requests variant={"default"} className=" border">
-              <RequestBody>
-                <HStack className="py-[7px]">
-                  <RequestImage />
-                </HStack>
-                <VStack className="gap-1">
-                  <RequestLabel>Question Papers</RequestLabel>
-                  <HStack>
-                    <RequestWeightLabel>25x20x25</RequestWeightLabel>
-                    <RequestWeightLabel>2Kg</RequestWeightLabel>
-                  </HStack>
-                  <RequestTimeLabel>
-                    <CalendarDays height={16} width={16} className="mr-1" />{" "}
-                    Requested 5 hours ago...
-                  </RequestTimeLabel>
-                </VStack>
-              </RequestBody>
-              <RequestButton />
-            </Requests>
-          </div>
-
-          <div className=" py-3.5">
-            <Requests variant={"default"} className=" border">
-              <RequestBody>
-                <HStack className="py-[7px]">
-                  <RequestImage />
-                </HStack>
-                <VStack className="gap-1">
-                  <RequestLabel>Question Papers</RequestLabel>
-                  <HStack>
-                    <RequestWeightLabel>25x20x25</RequestWeightLabel>
-                    <RequestWeightLabel>2Kg</RequestWeightLabel>
-                  </HStack>
-                  <RequestTimeLabel>
-                    <CalendarDays height={16} width={16} className="mr-1" />{" "}
-                    Requested 5 hours ago...
-                  </RequestTimeLabel>
-                </VStack>
-              </RequestBody>
-              <RequestButton />
-            </Requests>
-          </div>
-        </div>
       </div>
       <Rightbar />
     </div>
