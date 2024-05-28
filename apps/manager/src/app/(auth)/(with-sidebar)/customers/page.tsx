9c7e26478dbe4761c3d6cb3f3eefc60d809d68e7
x-sss-x <manu48167@gmail.com>
2024-05-28 11:03:33 +0530

    reform the structure and include supabase auth with it's clients and middleware

--- apps/manager/src/app/(auth)/(with-sidebar)/customers/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-  return (
-    <div>
-      {"Customer Table goes here (During Backend Integration Time) ..."}
-    </div>
-  );
-}
---
+++ apps/manager/src/utils/actions/auth.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { api } from "~/trpc/server";
+import { createClient } from "~/utils/server";
+
+export async function SigninWithGoogle() {
+  const supabase = createClient();
+  const origin =
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:${process.env.PORT}`
+      : headers().get("origin");
+  const redirectUrl = `${origin}/auth/callback?next=/dashboard`;
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: redirectUrl,
+    },
+  });
+  console.log("Auth Error", error, "URL", data.url);
+  if (data.url) redirect(data.url);
+}
+
+export async function cancelRequest({ id }: { id: string }) {
+  await api.packages.cancelRequest({ id });
+  revalidatePath("/", "layout");
+}
