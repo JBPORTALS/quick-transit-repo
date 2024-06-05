f073d75aa063b0dc68bdf0568cceca90945cc922
x-sss-x <manu48167@gmail.com>
2024-06-05 21:10:02 +0530

    "Update link error page text: 'Invitation Link is invalid or Expired' -> 'Link is invalid or Expired'"

--- apps/manager/src/app/(auth)/auth/link-error/page.tsx
+++ apps/manager/src/app/(auth)/auth/link-error/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <VStack className="h-full items-center justify-center">
       <MailWarningIcon className="size-32 text-primary/50" />
-      <Text styles={"h4"}>Invitation Link is invalid or Expired</Text>
+      <Text styles={"h4"}>Link is invalid or Expired</Text>
       <Text className="w-1/4 text-wrap text-center  text-accent-foreground/60">
         Your link has been expired or invalid. Please contact support team for
         verification of link.
