db5be2f9de055e1cca191e1800fd085a2b49916c
x-sss-x <manu48167@gmail.com>
2024-08-09 13:39:14 +0530

    fix the versions

--- apps/partner/src/app/(app)/(auth)/sign-in.tsx
+++ apps/partner/src/app/(app)/(auth)/sign-in.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
 
 export default function SignIn() {
-  return <View className="flex-1 items-center justify-center px-8"></View>;
+  return (
+    <View className="flex-1 items-center justify-center px-8">
+      <Button size={"lg"} className="w-full">
+        <Text>Sign In</Text>
+      </Button>
+    </View>
+  );
 }
