9b5d21705299658337829f2b89356fed0db94ee4
x-sss-x <jbportalsblr@gmail.com>
2025-01-03 11:53:09 +0530

    Update UI spacing and modify localhost IP; remove updateReview procedure

--- apps/qt-partner/lib/trpc/base-url.ts
+++ apps/qt-partner/lib/trpc/base-url.ts
@@ -15,7 +15,7 @@ export const getBaseUrl = () => {
    */
   // const debuggerHost = Constants.expoConfig?.hostUri;
 
-  const localhost = "192.168.17.111";
+  const localhost = "192.168.142.111";
 
   console.log(Constants.expoConfig);
   if (!localhost) {
