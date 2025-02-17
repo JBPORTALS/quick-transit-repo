e9f89b7e0fe6b136f69368e606257c15814a0cfb
x-sss-x <jbportalsblr@gmail.com>
2025-02-17 09:45:45 +0530

    feat: Enhance package display with empty state handling; improve search functionality layout

--- apps/qt-partner/app/(app)/(root)/(tabs)/index.tsx
+++ apps/qt-partner/app/(app)/(root)/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { RefreshControl, ScrollView, View } from "react-native";
+import { isEmpty } from "lodash";
 
 import {
   Card,
@@ -11,6 +12,8 @@ import {
 } from "~/components/ui/card";
 import { Progress } from "~/components/ui/progress";
 import { Text } from "~/components/ui/text";
+import { H3, Large, Lead, Muted } from "~/components/ui/typography";
+import { Bell } from "~/lib/icons/Bell";
 import { PackageIcon } from "~/lib/icons/PackageIcon";
 import { ActivityIndicator } from "~/lib/native/activity-indicator";
 import { api } from "~/lib/trpc/api";
@@ -32,89 +35,107 @@ export default function HomeScreen() {
   if (isLoading)
     return <ActivityIndicator size={45} className="mt-4 text-primary" />;
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl
-          refreshing={isFetching}
-          onRefresh={() => refreshData()}
-        />
-      }
-    >
-      <View className="flex-1 p-5">
-        <View className="w-full gap-3">
-          <View className="w-full max-w-full flex-row gap-3">
-            <Card className="w-full flex-shrink">
-              <CardHeader className="gap-2">
-                <CardDescription className="text-base">Today</CardDescription>
-                <CardTitle className="text-4xl">20</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Text className="text-sm text-muted-foreground">
-                  packages delivered.
-                </Text>
-              </CardContent>
-              {/* <CardFooter>
-            <Progress className="h-2" value={60} />
-          </CardFooter> */}
-            </Card>
-            <Card className="w-full flex-shrink">
-              <CardHeader className="gap-2">
-                <CardDescription className="text-base">Today</CardDescription>
-                <CardTitle className="text-4xl">30</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Text className="text-sm text-muted-foreground">
-                  packages in shipping.
-                </Text>
-              </CardContent>
-              {/* <CardFooter>
-            <Progress className="h-2" value={60} />
-          </CardFooter> */}
-            </Card>
+    <>
+      {isEmpty(data?.packages) ? (
+        <View className="h-[100vh] flex-1 items-center justify-center gap-3">
+          <Bell size={45} strokeWidth={1.5} className="text-muted-foreground" />
+          <View className="gap-0.5">
+            <Large className="text-center">No packages to deliver today</Large>
+            <Muted className="text-center">
+              If any packages should deliver today will be appear here
+            </Muted>
           </View>
-          <Card className="w-full">
-            <CardHeader className="gap-2">
-              <CardDescription className="text-base">Today</CardDescription>
-              <CardTitle className="text-4xl">20/50</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Text className="text-sm text-muted-foreground">
-                Still out of 50, 20 more packages to delivered.
-              </Text>
-            </CardContent>
-            <CardFooter>
-              <Progress style={{ height: 8 }} value={(20 / 50) * 100} />
-            </CardFooter>
-          </Card>
         </View>
-        <View className="gap-4">
-          <Text className="py-6 text-xl font-semibold text-foreground">
-            {"Today's Packages"}
-          </Text>
-          {data?.packages.map(({ id, package: packageDetails }) => (
-            <View
-              key={id.toString()}
-              className="w-full flex-grow-0 flex-row gap-2"
-            >
-              <View className="aspect-square min-w-20 max-w-20 items-center  justify-center rounded-md border border-border bg-muted/20">
-                <PackageIcon
-                  strokeWidth={1.25}
-                  size={32}
-                  className="text-muted-foreground"
-                />
+      ) : (
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={isFetching}
+              onRefresh={() => refreshData()}
+            />
+          }
+        >
+          <View className="flex-1 p-5">
+            <View className="w-full gap-3">
+              <View className="w-full max-w-full flex-row gap-3">
+                <Card className="w-full flex-shrink">
+                  <CardHeader className="gap-2">
+                    <CardDescription className="text-base">
+                      Today
+                    </CardDescription>
+                    <CardTitle className="text-4xl">20</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <Text className="text-sm text-muted-foreground">
+                      packages delivered.
+                    </Text>
+                  </CardContent>
+                  {/* <CardFooter>
+            <Progress className="h-2" value={60} />
+            </CardFooter> */}
+                </Card>
+                <Card className="w-full flex-shrink">
+                  <CardHeader className="gap-2">
+                    <CardDescription className="text-base">
+                      Today
+                    </CardDescription>
+                    <CardTitle className="text-4xl">30</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <Text className="text-sm text-muted-foreground">
+                      packages in shipping.
+                    </Text>
+                  </CardContent>
+                  {/* <CardFooter>
+            <Progress className="h-2" value={60} />
+            </CardFooter> */}
+                </Card>
               </View>
-              <View className="w-full flex-shrink">
-                <Text className="font-bold">{packageDetails.title}</Text>
-                <Text className="text-sm text-muted-foreground">
-                  {packageDetails.description.length > 60
-                    ? packageDetails.description.slice(0, 60).concat("...")
-                    : packageDetails.description}
-                </Text>
-              </View>
+              <Card className="w-full">
+                <CardHeader className="gap-2">
+                  <CardDescription className="text-base">Today</CardDescription>
+                  <CardTitle className="text-4xl">20/50</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Text className="text-sm text-muted-foreground">
+                    Still out of 50, 20 more packages to delivered.
+                  </Text>
+                </CardContent>
+                <CardFooter>
+                  <Progress style={{ height: 8 }} value={(20 / 50) * 100} />
+                </CardFooter>
+              </Card>
             </View>
-          ))}
-        </View>
-      </View>
-    </ScrollView>
+            <View className="gap-4">
+              <Text className="py-6 text-xl font-semibold text-foreground">
+                {"Today's Packages"}
+              </Text>
+              {data?.packages.map(({ id, package: packageDetails }) => (
+                <View
+                  key={id.toString()}
+                  className="w-full flex-grow-0 flex-row gap-2"
+                >
+                  <View className="aspect-square min-w-20 max-w-20 items-center  justify-center rounded-md border border-border bg-muted/20">
+                    <PackageIcon
+                      strokeWidth={1.25}
+                      size={32}
+                      className="text-muted-foreground"
+                    />
+                  </View>
+                  <View className="w-full flex-shrink">
+                    <Text className="font-bold">{packageDetails.title}</Text>
+                    <Text className="text-sm text-muted-foreground">
+                      {packageDetails.description.length > 60
+                        ? packageDetails.description.slice(0, 60).concat("...")
+                        : packageDetails.description}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </>
   );
 }
--- apps/qt-partner/app/(app)/(root)/(tabs)/packages.tsx
+++ apps/qt-partner/app/(app)/(root)/(tabs)/packages.tsx
@@ -6,10 +6,13 @@ import {
   View,
 } from "react-native";
 import { Link } from "expo-router";
+import { isEmpty } from "lodash";
 
 import { PackageItem } from "~/components/PackageItem";
 import { Input } from "~/components/ui/input";
 import { Separator } from "~/components/ui/separator";
+import { Large, Muted } from "~/components/ui/typography";
+import { PackageIcon } from "~/lib/icons/PackageIcon";
 import { SearchIcon } from "~/lib/icons/Search";
 import { ActivityIndicator } from "~/lib/native/activity-indicator";
 import { api } from "~/lib/trpc/api";
@@ -30,47 +33,65 @@ export default function PackagesIndex() {
   if (isLoading)
     return <ActivityIndicator size={45} className="mt-4 text-primary" />;
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl
-          style={{
-            backgroundColor: "red",
-          }}
-          refreshing={isFetching}
-          onRefresh={() => refreshData()}
-        />
-      }
-    >
-      <View className="flex-1 gap-5 p-5">
-        <View>
-          <Link href={"/search"} asChild>
-            <TouchableOpacity>
-              <View className="flex-row items-center overflow-hidden rounded-md border border-border bg-secondary pl-3">
-                <SearchIcon size={20} className="text-foreground" />
-                <Input
-                  placeholder="Search ..."
-                  readOnly
-                  className="native:h-10 w-full flex-shrink rounded-none border-0 bg-transparent"
-                />
-              </View>
-            </TouchableOpacity>
-          </Link>
+    <>
+      {isEmpty(data?.packages) ? (
+        <View className="h-[100vh] flex-1 items-center justify-center gap-3">
+          <PackageIcon
+            size={45}
+            strokeWidth={1.5}
+            className="text-muted-foreground"
+          />
+          <View className="gap-0.5">
+            <Large className="text-center">No packages assigend yet</Large>
+            <Muted className="text-center">
+              If assines the package, it will be appear here
+            </Muted>
+          </View>
         </View>
-        <View className="gap-3">
-          {data?.packages.map((data) => (
-            <Link
-              key={data.id.toString()}
-              asChild
-              href={`/package/${data.package_id}`}
-            >
-              <TouchableOpacity className="gap-3">
-                <PackageItem key={data.id} data={data} />
-                <Separator key={data.id + "sperator"} />
-              </TouchableOpacity>
-            </Link>
-          ))}
-        </View>
-      </View>
-    </ScrollView>
+      ) : (
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              style={{
+                backgroundColor: "red",
+              }}
+              refreshing={isFetching}
+              onRefresh={() => refreshData()}
+            />
+          }
+        >
+          <View className="flex-1 gap-5 p-5">
+            <View>
+              <Link href={"/search"} asChild>
+                <TouchableOpacity>
+                  <View className="flex-row items-center overflow-hidden rounded-md border border-border bg-secondary pl-3">
+                    <SearchIcon size={20} className="text-foreground" />
+                    <Input
+                      placeholder="Search ..."
+                      readOnly
+                      className="native:h-10 w-full flex-shrink rounded-none border-0 bg-transparent"
+                    />
+                  </View>
+                </TouchableOpacity>
+              </Link>
+            </View>
+            <View className="gap-3">
+              {data?.packages.map((data) => (
+                <Link
+                  key={data.id.toString()}
+                  asChild
+                  href={`/package/${data.package_id}`}
+                >
+                  <TouchableOpacity className="gap-3">
+                    <PackageItem key={data.id} data={data} />
+                    <Separator key={data.id + "sperator"} />
+                  </TouchableOpacity>
+                </Link>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </>
   );
 }
