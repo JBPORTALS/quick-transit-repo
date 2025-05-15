e3222f4930d664be3755a23d2f3ff99c7137cb3c
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-05-15 16:56:20 +0530

    feat: Implement camera functionality with file upload to Supabase; enhance state management for upload status

--- apps/qt-partner/lib/useStorage.tsx
+++ apps/qt-partner/lib/useStorage.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
+import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
+import { decode } from "base64-arraybuffer";
 
-export function useCamera() {
-  const [isCameraLoading, setCameraLoading] = useState(false);
+import { supabase } from "./supabase";
 
-  const [file, setFile] = useState();
+export function useCamera(
+  bucket: string,
+  path: string,
+  options: {
+    onSuccess: (res: { id: string; path: string; fullPath: string }) => void;
+    onError: () => void;
+  },
+) {
+  const [isUploading, setIsUploading] = useState(false);
 
   const openCamera = async () => {
-    setCameraLoading(true);
+    setIsUploading(true);
     // Request camera permissions
     const { status } = await ImagePicker.requestCameraPermissionsAsync();
     if (status !== "granted") {
@@ -24,8 +33,22 @@ export function useCamera() {
 
     if (!result.canceled) {
       const fileAsset = result.assets.at(0);
-      if (fileAsset) setFile();
+
+      if (fileAsset) {
+        const body = await FileSystem.readAsStringAsync(fileAsset.uri, {
+          encoding: "base64",
+        });
+
+        const res = await supabase.storage
+          .from(bucket)
+          .upload(`${path}`, decode(body), { upsert: true });
+
+        if (res.error) options.onError();
+        if (res.data) options.onSuccess(res.data);
+      }
     }
-    setCameraLoading(false);
+    setIsUploading(false);
   };
+
+  return { isUploading, openCamera };
 }
